4d0cdeb6ed538fb20584d02de333178534f509f5
JayChadha-uva <89432060+JayChadha-uva@users.noreply.github.com>
2023-02-07 09:37:10 -0500

    Dropdown Search working

--- src/App.js
+++ src/App.js
@@ -3,130 +3,85 @@ import "./App.css";
 
 import Discover from "./Discover";
 import SelectSearch from "react-select-search";
+import "react-select-search/style.css";
 
 import React, { useEffect, useState } from "react";
 
 function App() {
   const [user, setUser] = useState([]);
-  const [message, setMessage] = useState("");
+  const [idVal, setIdVal] = useState(null);
 
   const img_URL = "https://image.tmdb.org/t/p/original";
   const API_KEY = "3d0ac201ad49d76eb1e30e54903dcc54";
 
-  function getOptions(query) {
-    return new Promise((resolve, reject) => {
-      fetch(
-        "https://api.themoviedb.org/3/search/movie?api_key=" +
-          API_KEY +
-          "&query=" +
-          String(message).replace(/ /g, "+")
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          resolve(
-            data.results.map((obj) => ({
-              value: obj.original_title,
-              name: obj.original_title,
-            }))
-          );
-        })
-        .catch(reject);
-    });
-  }
-
-  const fetchData = () => {
+  const fetchMovie = () => {
     return fetch(
-      "https://api.themoviedb.org/3/search/movie?api_key=" +
-        API_KEY +
-        "&query=" +
-        String(message).replace(/ /g, "+")
+      `https://api.themoviedb.org/3/movie/${idVal}?api_key=${API_KEY}`
     )
       .then((response) => response.json())
-      .then((data) => setUser(data.results));
+      .then((data) => setUser(data));
   };
 
   useEffect(() => {
-    fetchData();
-  }, []);
-
-  const handleChange = (event) => {
-    setMessage(event.target.value);
-    setUser([]);
-    fetchData();
-  };
+    fetchMovie();
+  }, [idVal]);
 
   return (
     <>
       <nav class="navbar sticky-top bg-body-tertiary">
         <div class="container-md my-2">
           <a class="navbar-brand h1">Movie Rater</a>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="text"
-              id="message"
-              name="message"
-              placeholder="Search"
-              onChange={handleChange}
-              value={message}
-            />
-          </form>
+          <SelectSearch
+            options={[]}
+            getOptions={(query) => {
+              return new Promise((resolve, reject) => {
+                fetch(
+                  `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${String(
+                    query
+                  ).replace(/ /g, "+")}`
+                )
+                  .then((response) => response.json())
+                  .then((movies) => {
+                    resolve(
+                      movies.results.map(({ original_title, id }) => ({
+                        value: id,
+                        name: original_title,
+                      }))
+                    );
+                  })
+                  .catch(reject);
+              });
+            }}
+            search
+            onChange={setIdVal}
+            placeholder="Search Movies"
+          />
         </div>
       </nav>
-
-      <SelectSearch
-        options={[]}
-        defaultValue="None"
-        getOptions={(query) => {
-          return new Promise((resolve, reject) => {
-            fetch(
-              `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${String(
-                query
-              ).replace(/ /g, "+")}`
-            )
-              .then((response) => response.json())
-              .then((drinks) => {
-                resolve(
-                  drinks.results.map(({ original_title }) => ({
-                    value: original_title,
-                    name: original_title,
-                  }))
-                );
-              })
-              .catch(reject);
-          });
-        }}
-        search
-        placeholder="Search Movies"
-      />
 
       <div>
         <div class="container container-md">
           <div class="row row-cols-2">
-            {user &&
-              user.length > 0 &&
-              user.map((userObj, index) => (
-                <div class="col">
-                  <div class="card my-4" key={userObj.original_title}>
-                    <div class="row ">
-                      <div class="col-4">
-                        <img
-                          src={img_URL + userObj.poster_path}
-                          class="card-img-top"
-                          alt="..."
-                        />
-                      </div>
-                      <div class="card-body col-8">
-                        <div class="d-flex justify-content-between">
-                          <h5 class="card-title">{userObj.original_title}</h5>
-                          <p class="card-text me-5">{userObj.release_date}</p>
-                        </div>
-                        <p class="card-text">{userObj.overview}</p>
-                      </div>
+            <div class="col">
+              <div class="card my-4" key={user.original_title}>
+                <div class="row ">
+                  <div class="col-4">
+                    <img
+                      src={img_URL + user.poster_path}
+                      class="card-img-top"
+                      alt="..."
+                    />
+                  </div>
+                  <div class="card-body col-8">
+                    <div class="d-flex justify-content-between">
+                      <h5 class="card-title">{user.original_title}</h5>
+                      <p class="card-text me-5">{user.release_date}</p>
                     </div>
+                    <p class="card-text">{user.overview}</p>
                   </div>
                 </div>
-              ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
